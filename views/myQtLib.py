13113a03d58abbf620549c855fa1301cd9f9aa2f
Flare <zhao_tianren@163.com>
2018-06-17 21:33:27 +0800

    重构all_component

--- views/myQtLib.py
+++ views/myQtLib.py
@@ -130,133 +130,134 @@ class MainWindow(QtGui.QMainWindow):
         data_menu.addAction(insert_weather)
 
     def create_user_fuc(self):
-        create_user_component_list = []
-        self.myLabel_userType = MyLabel("用户类型 : ", self)
-        self.myLabel_userType.move(100, 60)
-        create_user_component_list.append(self.myLabel_userType)
+        create_user_component_dict = {}
+        myLabel_userType = MyLabel("用户类型 : ", self)
+        myLabel_userType.move(100, 60)
+        create_user_component_dict['myLabel_userType'] = myLabel_userType
 
-        self.myLineEdit_userType = MyLineEdit(self)
-        self.myLineEdit_userType.move(200, 60)
-        create_user_component_list.append(self.myLineEdit_userType)
+        myLineEdit_userType = MyLineEdit(self)
+        myLineEdit_userType.move(200, 60)
+        create_user_component_dict['myLineEdit_userType'] = myLineEdit_userType
 
-        self.myLabel_userName = MyLabel("用户名称 : ", self)
-        self.myLabel_userName.move(100, 120)
-        create_user_component_list.append(self.myLabel_userName)
+        myLabel_userName = MyLabel("用户名称 : ", self)
+        myLabel_userName.move(100, 120)
+        create_user_component_dict['myLabel_userName'] = myLabel_userName
 
-        self.myLineEdit_userName = MyLineEdit(self)
-        self.myLineEdit_userName.move(200, 120)
-        create_user_component_list.append(self.myLineEdit_userName)
+        myLineEdit_userName = MyLineEdit(self)
+        myLineEdit_userName.move(200, 120)
+        create_user_component_dict['myLineEdit_userName'] = myLineEdit_userName
 
-        self.myLabel_gasUnit = MyLabel("气量单位 : ", self)
-        self.myLabel_gasUnit.move(100, 180)
-        create_user_component_list.append(self.myLabel_gasUnit)
+        myLabel_gasUnit = MyLabel("气量单位 : ", self)
+        myLabel_gasUnit.move(100, 180)
+        create_user_component_dict['myLabel_gasUnit'] = myLabel_gasUnit
 
-        self.myLineEdit_gasUnit = MyLineEdit(self)
-        self.myLineEdit_gasUnit.move(200, 180)
-        create_user_component_list.append(self.myLineEdit_gasUnit)
+        myLineEdit_gasUnit = MyLineEdit(self)
+        myLineEdit_gasUnit.move(200, 180)
+        create_user_component_dict['myLineEdit_gasUnit'] = myLineEdit_gasUnit
 
-        self.myLabel_userUnit = MyLabel("用户单位 : ", self)
-        self.myLabel_userUnit.move(100, 240)
-        create_user_component_list.append(self.myLabel_userUnit)
+        myLabel_userUnit = MyLabel("用户单位 : ", self)
+        myLabel_userUnit.move(100, 240)
+        create_user_component_dict['myLabel_userUnit'] = myLabel_userUnit
 
-        self.myLineEdit_userUnit = MyLineEdit(self)
-        self.myLineEdit_userUnit.move(200, 240)
-        create_user_component_list.append(self.myLineEdit_userUnit)
+        myLineEdit_userUnit = MyLineEdit(self)
+        myLineEdit_userUnit.move(200, 240)
+        create_user_component_dict['myLineEdit_userUnit'] = myLineEdit_userUnit
 
-        self.myLabel_beizhu = MyLabel("备注 : ", self)
-        self.myLabel_beizhu.move(100, 300)
-        create_user_component_list.append(self.myLabel_beizhu)
+        myLabel_beizhu = MyLabel("备注 : ", self)
+        myLabel_beizhu.move(100, 300)
+        create_user_component_dict['myLabel_beizhu'] = myLabel_beizhu
 
-        self.myLineEdit_beizhu = MyLineEdit(self)
-        self.myLineEdit_beizhu.move(200, 300)
-        self.myLineEdit_beizhu.resize(400, 30)
-        create_user_component_list.append(self.myLineEdit_beizhu)
+        myLineEdit_beizhu = MyLineEdit(self)
+        myLineEdit_beizhu.move(200, 300)
+        myLineEdit_beizhu.resize(400, 30)
+        create_user_component_dict['myLineEdit_beizhu'] = myLineEdit_beizhu
 
-        self.myButton_insertUser = MyButton("确认", self)
-        self.myButton_insertUser.move(100, 360)
-        create_user_component_list.append(self.myButton_insertUser)
+        myButton_insertUser = MyButton("确认", self)
+        myButton_insertUser.move(100, 360)
+        create_user_component_dict['myButton_insertUser'] = myButton_insertUser
 
-        self.connect(self.myButton_insertUser, QtCore.SIGNAL("clicked()"), self.insert_user)
+        self.connect(create_user_component_dict['myButton_insertUser'], QtCore.SIGNAL("clicked()"), self.insert_user)
 
-        self.all_component["create_user"] = create_user_component_list
-        for x in create_user_component_list:
-            x.hide()
+        self.all_component["create_user"] = create_user_component_dict
+        for x in create_user_component_dict:
+            create_user_component_dict[x].hide()
 
     def insert_weather_fuc(self):
-        insert_weather_component_list = []
+        insert_weather_component_dict = {}
 
-        self.myLabel_date = MyLabel('日期：', self)
-        self.myLabel_date.move(100, 60)
-        insert_weather_component_list.append(self.myLabel_date)
-
-        # self.myComboBox_date = MyComboBox(['1996/1/2', '1996/1/27'], self)
-        # self.myComboBox_date.move(200, 60)
-        # insert_weather_component_list.append(self.myComboBox_date)
+        myLabel_date = MyLabel('日期：', self)
+        myLabel_date.move(100, 60)
+        insert_weather_component_dict['myLabel_date'] = myLabel_date
 
         now_day = time.strftime("%Y-%m-%d", time.localtime())
-        self.myDateEdit_date = MyDateEdit(self, now_day)
-        self.myDateEdit_date.move(200, 60)
-        insert_weather_component_list.append(self.myDateEdit_date)
+        myDateEdit_date = MyDateEdit(self, now_day)
+        myDateEdit_date.move(200, 60)
+        insert_weather_component_dict['myDateEdit_date'] = myDateEdit_date
 
-        self.myLabel_maxTemperature = MyLabel('最高气温：', self)
-        self.myLabel_maxTemperature.move(100, 120)
-        insert_weather_component_list.append(self.myLabel_maxTemperature)
+        myLabel_maxTemperature = MyLabel('最高气温：', self)
+        myLabel_maxTemperature.move(100, 120)
+        insert_weather_component_dict['myLabel_maxTemperature'] = myLabel_maxTemperature
 
-        self.myLineEdit_maxTemperature = MyLineEdit(self)
-        self.myLineEdit_maxTemperature.move(200, 120)
-        insert_weather_component_list.append(self.myLineEdit_maxTemperature)
+        myLineEdit_maxTemperature = MyLineEdit(self)
+        myLineEdit_maxTemperature.move(200, 120)
+        insert_weather_component_dict['myLineEdit_maxTemperature'] = myLineEdit_maxTemperature
 
-        self.myLabel_maxTemperatureCentigrade = MyLabel('℃', self)
-        self.myLabel_maxTemperatureCentigrade.move(300, 120)
-        insert_weather_component_list.append(self.myLabel_maxTemperatureCentigrade)
+        myLabel_maxTemperatureCentigrade = MyLabel('℃', self)
+        myLabel_maxTemperatureCentigrade.move(300, 120)
+        insert_weather_component_dict['myLabel_maxTemperatureCentigrade'] = myLabel_maxTemperatureCentigrade
 
-        self.myLabel_minTemperature = MyLabel('最低气温：', self)
-        self.myLabel_minTemperature.move(100, 180)
-        insert_weather_component_list.append(self.myLabel_minTemperature)
+        myLabel_minTemperature = MyLabel('最低气温：', self)
+        myLabel_minTemperature.move(100, 180)
+        insert_weather_component_dict['myLabel_minTemperature'] = myLabel_minTemperature
 
-        self.myLineEdit_minTemperature = MyLineEdit(self)
-        self.myLineEdit_minTemperature.move(200, 180)
-        insert_weather_component_list.append(self.myLineEdit_minTemperature)
+        myLineEdit_minTemperature = MyLineEdit(self)
+        myLineEdit_minTemperature.move(200, 180)
+        insert_weather_component_dict['myLineEdit_minTemperature'] = myLineEdit_minTemperature
 
-        self.myLabel_minTemperatureCentigrade = MyLabel('℃', self)
-        self.myLabel_minTemperatureCentigrade.move(300, 180)
-        insert_weather_component_list.append(self.myLabel_minTemperatureCentigrade)
+        myLabel_minTemperatureCentigrade = MyLabel('℃', self)
+        myLabel_minTemperatureCentigrade.move(300, 180)
+        insert_weather_component_dict['myLabel_minTemperatureCentigrade'] = myLabel_minTemperatureCentigrade
 
-        self.myLabel_avgTemperature = MyLabel('平均气温：', self)
-        self.myLabel_avgTemperature.move(100, 240)
-        insert_weather_component_list.append(self.myLabel_avgTemperature)
+        myLabel_avgTemperature = MyLabel('平均气温：', self)
+        myLabel_avgTemperature.move(100, 240)
+        insert_weather_component_dict['myLabel_avgTemperature'] = myLabel_avgTemperature
 
-        self.myLineEdit_avgTemperature = MyLineEdit(self)
-        self.myLineEdit_avgTemperature.move(200, 240)
-        insert_weather_component_list.append(self.myLineEdit_avgTemperature)
+        myLineEdit_avgTemperature = MyLineEdit(self)
+        myLineEdit_avgTemperature.move(200, 240)
+        insert_weather_component_dict['myLineEdit_avgTemperature'] = myLineEdit_avgTemperature
 
-        self.myLabel_avgTemperatureCentigrade = MyLabel('℃', self)
-        self.myLabel_avgTemperatureCentigrade.move(300, 240)
-        insert_weather_component_list.append(self.myLabel_avgTemperatureCentigrade)
+        myLabel_avgTemperatureCentigrade = MyLabel('℃', self)
+        myLabel_avgTemperatureCentigrade.move(300, 240)
+        insert_weather_component_dict['myLabel_avgTemperatureCentigrade'] = myLabel_avgTemperatureCentigrade
 
-        self.myButton_insertWeather = MyButton("确认", self)
-        self.myButton_insertWeather.move(100, 300)
-        insert_weather_component_list.append(self.myButton_insertWeather)
+        myButton_insertWeather = MyButton("确认", self)
+        myButton_insertWeather.move(100, 300)
+        insert_weather_component_dict['myButton_insertWeather'] = myButton_insertWeather
 
-        self.connect(self.myButton_insertWeather, QtCore.SIGNAL("clicked()"), self.insert_weather)
+        self.connect(insert_weather_component_dict['myButton_insertWeather'], QtCore.SIGNAL("clicked()"), self.insert_weather)
 
-        self.all_component["insert_weather"] = insert_weather_component_list
-        for x in insert_weather_component_list:
-            x.hide()
+        self.all_component["insert_weather"] = insert_weather_component_dict
+        for x in insert_weather_component_dict:
+            insert_weather_component_dict[x].hide()
+
+
+    def insert_user_data_fuc(self):
+        insert_user_data_component_dict = {}
+
 
     def display_create_user(self):
         for k in self.all_component:
             for x in self.all_component[k]:
-                x.hide()
+                self.all_component[k][x].hide()
         for x in self.all_component["create_user"]:
-            x.show()
+            self.all_component["create_user"][x].show()
 
     def display_insert_weather(self):
         for k in self.all_component:
             for x in self.all_component[k]:
-                x.hide()
+                self.all_component[k][x].hide()
         for x in self.all_component['insert_weather']:
-            x.show()
+            self.all_component['insert_weather'][x].show()
 
     def center(self):
         screen = QtGui.QDesktopWidget().screenGeometry()
@@ -264,11 +265,11 @@ class MainWindow(QtGui.QMainWindow):
         self.move((screen.width() - size.width()) / 2, (screen.height() - size.height()) / 2)
 
     def insert_user(self):
-        userType = self.myLineEdit_userType.text()
-        userName = self.myLineEdit_userName.text()
-        gasUnit = self.myLineEdit_gasUnit.text()
-        userUnit = self.myLineEdit_userUnit.text()
-        remark = self.myLineEdit_beizhu.text()
+        userType = self.all_component['create_user']['myLineEdit_userType'].text()
+        userName = self.all_component['create_user']['myLineEdit_userName'].text()
+        gasUnit = self.all_component['create_user']['myLineEdit_gasUnit'].text()
+        userUnit = self.all_component['create_user']['myLineEdit_userUnit'].text()
+        remark = self.all_component['create_user']['myLineEdit_beizhu'].text()
         if userUnit and userName and gasUnit and userType:
             res = add_user(userType, userName, gasUnit, userUnit)
             if res[0]:
@@ -279,11 +280,11 @@ class MainWindow(QtGui.QMainWindow):
             print("you can't")
 
     def insert_weather(self):
-        date = self.myDateEdit_date.text()
+        date = self.all_component['insert_weather']['myDateEdit_date'].text()
         try:
-            maxTemperature = float(self.myLineEdit_maxTemperature.text())
-            minTemperature = float(self.myLineEdit_minTemperature.text())
-            avgTemperature = float(self.myLineEdit_avgTemperature.text())
+            maxTemperature = float(self.all_component['insert_weather']['myLineEdit_maxTemperature'].text())
+            minTemperature = float(self.all_component['insert_weather']['myLineEdit_minTemperature'].text())
+            avgTemperature = float(self.all_component['insert_weather']['myLineEdit_avgTemperature'].text())
             if int(time.time()) < datetime_to_timestamp(date, '%Y/%m/%d'):
                 QtGui.QMessageBox.warning(self, "天气录入失败", "不能录入未来日期的天气", "确定")
             elif date and maxTemperature and minTemperature and avgTemperature:
